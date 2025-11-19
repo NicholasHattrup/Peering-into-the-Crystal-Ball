ed26d153d5d359c17fb12979b98db7adc7fb2a75
NicholasHattrup <nhattrup@andrew.cmu.edu>
2025-11-19 15:17:22 -0500

    figures 8 and 9

---
+++ fig_8_and_9/process_jobs.py
@@ -0,0 +1,236 @@
+import numpy as np
+import os, sys
+from scipy.stats import norm
+import scipy.sparse as sp
+from numba import jit
+import pandas as pd
+import matplotlib.pyplot as plt
+import scienceplots
+plt.style.use(['science', 'no-latex'])
+
+def load_dump(file_name, columns, ids=True):
+    with open(file_name, 'r') as f:
+        # Read the entire file into memory
+        lines = f.readlines()
+
+    num_atoms = None
+    header_size = 0
+
+    # Parse the header to find number of atoms and header size
+    for i, line in enumerate(lines):
+        line = line.strip()
+        if 'ITEM: NUMBER OF ATOMS' in line:
+            num_atoms = int(lines[i + 1].strip())
+        elif 'ITEM: ATOMS' in line:
+            header_size = i + 1
+            break
+
+    if num_atoms is None:
+        raise ValueError("Number of atoms not found in the file header.")
+
+    # Calculate lines per configuration
+    lines_per_config = header_size + num_atoms
+
+    # Calculate the number of configurations
+    total_lines = len(lines)
+    num_configs = total_lines // lines_per_config
+
+    # Preallocate the array
+    data = np.zeros((num_configs, num_atoms, len(columns)))
+    start_idx = header_size
+    end_idx = start_idx + num_atoms
+
+    if ids:
+        num_features = len(columns) + 1
+    else:
+        num_features = len(columns)
+    
+
+    # Extract configurations
+    if ids: 
+        for config_index in range(num_configs):
+            config_data = np.fromstring("\n".join(lines[start_idx:end_idx]), sep=' ').reshape(-1, num_features)
+            data[config_index, config_data[:, 0].astype(int) - 1] = config_data[:, 1:]
+            start_idx += lines_per_config
+            end_idx += lines_per_config
+    else:
+        for config_index in range(num_configs):
+            config_data = np.fromstring("\n".join(lines[start_idx:end_idx]), sep=' ').reshape(-1, num_features)
+            data[config_index] = config_data
+            start_idx += lines_per_config
+            end_idx += lines_per_config
+    return data
+def parseRDF(file):
+    with open(file) as f:
+        f.readline()
+        f.readline()
+        f.readline()
+        _, bins = f.readline().split()
+        bins = int(bins)
+        length = len(f.readlines()) + 1
+    samples = length // (bins+1)
+    times = np.empty(samples)
+    distances = np.empty(bins)
+    rdf = np.empty((samples, bins))
+    with open(file) as f:
+        data = f.readlines()
+        offSet = 3
+        idx = 0
+        for i in range(offSet, length, bins+1):
+            times[idx] = float(data[i].split()[0])
+            for j in range(i+1, i+bins+1):
+                rdf[idx, j-i-1] = float(data[j].split()[-1]) # Normalized against density
+            idx += 1
+        for i in range(offSet+1, offSet+bins+1):
+            distances[i-offSet-1] = float(data[i].split()[1]) # Actual values
+    return times, distances, rdf
+
+
+
+def main():
+    timestep = 0.002
+    rho_vals = sys.argv[1].split(',')
+    T_vals = sys.argv[2].split(',')
+    num_jobs = int(sys.argv[3])
+    NUM_ROWS = 3 # THIS IS HARD CODED ASSUMING len(rho_vals) == len(T_vals) == 9
+    NUM_COLS = 3
+
+    directories = [f'rho_{rho}_T_{T}' for rho, T in zip(rho_vals, T_vals)]
+
+    diffusions = []
+    msds_all = []
+    rdfs_all = []
+
+    for directory in directories:
+        msds = [] 
+        for i in range(1, num_jobs + 1):
+            msd_df = pd.read_csv(directory + f'/job_{i}/msd_{i}.dat', comment='#', delim_whitespace=True, names=['t', 'msd'])
+            t = msd_df['t'].values * timestep
+            msd = msd_df['msd'].values
+            msds.append(msd)
+
+        msd_avg = np.array(msds).mean(axis=0)
+        t -= t[0]
+        msd_avg[0] = 0 
+
+        D = (msd_avg @ t / (t @ t)) / 6
+        diffusions.append(D)
+        msds_all.append(msd_avg)
+
+    log_times = np.log(t)
+    log_min = np.min(log_times[1:])
+    log_max = np.max(log_times)
+    points = np.linspace(log_min, log_max, 16)
+    # Get log_times closest to points
+    closest_points = []
+    for point in points:
+        closest_points.append(np.argmin(np.abs(log_times - point)))
+
+    for directory in directories:
+        rdfs = []
+        for i in range(1, num_jobs + 1):
+            _, dists, rdf = parseRDF(directory + '/rdf/rdf.dat')
+            rdfs.append(rdf)
+        rdf_avg = np.mean(rdfs, axis=0)
+        rdfs_all.append(rdfs)
+    
+
+    # RDF figure 
+    fig, axs = plt.subplots(3, 3, figsize=(18, 6))
+
+
+    y_min_all = np.inf
+    y_max_all = -np.inf
+
+    sigma_peaks = []
+    for i in range(NUM_ROWS):
+        for j in range(NUM_COLS):
+            idx = 3*i+j
+            for rdfs in rdfs_all[idx]:
+                for rdf in rdfs:
+                    axs[i, j].plot(dists, rdf, color='gainsboro', alpha=0.05)
+            axs[i, j].plot(dists, rdf_avg[idx], color='cornflowerblue', alpha=1, linewidth=2.5, linestyle='-.')
+            peak = np.argmax(rdf_avg[idx])
+            sigma_peaks.append(dists[peak])
+            # Draw vertical line at peak
+            axs[i, j].axvline(x=dists[peak], color='black', linestyle=':', alpha=0.75, linewidth=1.5)
+            axs[i, j].text(1.5, 4, r'$r_{\mathrm{peak}}=%.3f \, \sigma$' % dists[peak], fontsize=16, ha='center', va='center', color='black')
+
+
+            y_min, y_max = axs[i, j].get_ylim()
+            y_min_all = min(y_min, y_min_all)
+            y_max_all = max(y_max, y_max_all)
+            axs[i, j].set_xlim(0, 2)
+            axs[i, j].set_yticks([0, 1, 2, 3, 4])
+            axs[i, j].set_yticklabels([0, 1, 2, 3, 4], fontsize=16)
+            axs[i, j].set_xticks([0, 0.5, 1, 1.5, 2])
+            axs[i, j].set_xticklabels([0, 0.5, 1, 1.5, 2], fontsize=16)
+            axs[-1, j].set_xlabel(r'$r/\sigma$', fontsize=24)
+            axs[i, 0].set_ylabel(r'$g(r)$', fontsize=24)
+            temp, rho = rho_vals[idx], T_vals[idx]
+            axs[i, j].set_title(r'$T^{*} = %.2f,  \,  \rho^{*} = %.2f$' % (temp, rho), fontsize=18, pad=10)
+            
+
+    for i in range(3):
+        for j in range(3):
+            axs[i, j].set_ylim(y_min_all, y_max_all)
+
+    plt.tight_layout()
+    plt.savefig('rdfs.png', dpi=300, bbox_inches='tight')
+
+    plt.clf()
+    y_min_all = np.inf 
+    y_max_all = -np.inf
+
+
+    for i in range(3):
+        for j in range(3):
+            idx = 3*i+j
+            axs[i, j].plot(t[closest_points]/diffusions[idx],
+                        msds_all[idx, closest_points],
+                        marker='o',
+                        markersize=8,
+                        markerfacecolor='white',
+                        markeredgecolor='black',
+                        markeredgewidth=1.25,
+                        color='black',   # line color
+                        linewidth=.75,
+                        zorder=2,
+                        alpha=.9,
+                        label='MD')
+            y_min, y_max = axs[i, j].get_ylim()
+            y_min_all = min(y_min, y_min_all)
+            y_max_all = max(y_max, y_max_all)
+
+            axs[i, j].set_xscale('log')
+            axs[i, j].set_yscale('log')
+            axs[-1, j].set_xlabel(r'$t/t_{ \mathrm{D} }$', fontsize=24)
+            axs[i, 0].set_ylabel('MSD', fontsize=24)
+
+            x_min, x_max = axs[i, j].get_xlim()
+            axs[i, j].set_xlim(1e-5, x_max)
+            temp, rho = rho_vals[idx], T_vals[idx]
+            axs[i, j].set_title(r'$T^{*} = %.2f,  \,  \rho^{*} = %.2f$' % (temp, rho), fontsize=18, pad=10)
+
+            t_B = (2**(1/6)-sigma_peaks[idx])/np.sqrt(3*temp)
+            t_B *= diffusions[idx]
+
+            t_L = (1/(np.pi * sigma_peaks[idx]**2 * rho))/np.sqrt(3*temp)
+            t_L *= diffusions[idx]
+            axs[i, j].axvspan(1e-5, t_B,color='mediumseagreen', alpha=0.12)
+            axs[i, j].axvspan(t_L, x_max, color='mediumpurple', alpha=0.12)
+
+    for i in range(3):
+        for j in range(3):
+            axs[i, j].set_ylim(1e-5, y_max_all*2)
+            axs[i, j].tick_params(axis='both', which='major', labelsize=14)
+
+    plt.tight_layout()
+
+    # Save fig as svg
+    plt.savefig('msd_regions.png', dpi=300, bbox_inches='tight')
+
+
+
+
+    
